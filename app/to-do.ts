a3f4bf1246b14a239879f5d1cb930e7dda39bc04
Michelle Poterek <poterekm@gmail.com>
2017-08-13 21:09:33 -0700

    call markDone on first instantiation of task

--- app/to-do.ts
+++ app/to-do.ts
@@ -2,7 +2,7 @@ class Task {
   done: boolean = false;
 
   constructor(public description: string, public priority: string){}
-  
+
   markDone(){
     this.done = true;
   }
@@ -12,6 +12,8 @@ var tasks: Task[] = [];
 tasks.push(new Task('Do the dishes.', 'Medium'));
 tasks.push(new Task('Buy chocolate.', 'Low'));
 tasks.push(new Task('Do laundry.', 'High'));
+
+tasks[0].markDone();
 
 for(var task of tasks){
   console.log(task);
